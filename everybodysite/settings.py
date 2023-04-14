b2bcc4feaaf7b9b951d526209229cc00a8de9a02
aparks4 <arynmparks@gmail.com>
2023-04-14 15:54:54 -0700

    installs dependencies and updates requirements.txt

--- everybodysite/settings.py
+++ everybodysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'django-cors-headers',
     'rest_framework',
     'rest_framework_simplejwt',
     'main_app',
