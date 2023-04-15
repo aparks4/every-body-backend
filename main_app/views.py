da984ad25d6a3717f525a3b08dc941ec32c185f9
aparks4 <arynmparks@gmail.com>
2023-04-14 17:59:52 -0700

    comments out unused imports in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,16 @@
 from .models import TeamMember
 
-from rest_framework import generics, status
+# from rest_framework import generics, status
 from .serializers import TeamMemberSerializer
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http import JsonResponse
-from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.views import APIView
-from rest_framework.decorators import permission_classes
-from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
-from rest_framework import viewsets
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from django.http import JsonResponse
+# from django.contrib.auth.models import User
+# from rest_framework.permissions import AllowAny, IsAuthenticated
+# from rest_framework.views import APIView
+# from rest_framework.decorators import permission_classes
+# from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
+# from rest_framework import viewsets
 # Create your views here.
 
 
