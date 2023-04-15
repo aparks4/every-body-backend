f3392a039be279a5cbba9f980eaf1da0e304831b
aparks4 <arynmparks@gmail.com>
2023-04-14 18:12:39 -0700

    comments out api-auth path in main_app/urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from . import views
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('team', views.TeamMemberList.as_view(), name="team_member_list"),
     path('team/<int:pk>', views.TeamMemberDetail.as_view(), name="team_member_detail"),
     
