70e553b3c5d3bd5a04f8d24dddd645ee352e0cc4
aparks4 <arynmparks@gmail.com>
2023-04-15 16:17:15 -0700

    urls.py config

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-    path('team', views.TeamMemberList.as_view(), name="team_member_list"),
+    path('team/', views.TeamMemberList.as_view(), name="team_member_list"),
     path('team/<int:pk>', views.TeamMemberDetail.as_view(), name="team_member_detail"),
     
 ]
