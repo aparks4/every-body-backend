29b2cd66f16be09c57003fbcb1b2d993c75608a7
aparks4 <arynmparks@gmail.com>
2023-08-12 21:08:02 -0700

    RetreatAdmin model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import TeamMember, Retreat
 
+class RetreatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'dates')  # Fields to display in the list view
+    list_filter = ('name', 'dates')  # Fields to use for filtering in the right sidebar
+    search_fields = ('name', 'dates')  # Fields to search for using the search bar
+
 # Register your models here.
 admin.site.register(TeamMember)
-admin.site.register(Retreat)
+admin.site.register(Retreat, RetreatAdmin)
