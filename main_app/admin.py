79b38a386004eeebf9d870fef9822345871200c1
aparks4 <arynmparks@gmail.com>
2023-08-12 21:01:20 -0700

    retreat model

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import TeamMember, Retreat
 
 # Register your models here.
-admin.site.register(TeamMember, Retreat)
+admin.site.register(TeamMember)
+admin.site.register(Retreat)
