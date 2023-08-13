09dcf05ab82f0de24715c0099b95a1da8a600763
aparks4 <arynmparks@gmail.com>
2023-08-12 18:11:03 -0700

    register Retreat model for admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TeamMember
+from .models import TeamMember, Retreat
 
 # Register your models here.
 admin.site.register(TeamMember)
+admin.site.register(Retreat)
